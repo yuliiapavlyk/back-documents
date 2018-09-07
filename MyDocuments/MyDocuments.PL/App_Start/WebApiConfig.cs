49b607a0eec5c4927a9b9690aca9c16ef2f8bf18
ALysyi <andrii.lysyi1@gmail.com>
2018-09-07 12:48:10 +0300

    created project

---
+++ MyDocuments/MyDocuments.PL/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace MyDocuments.PL
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            string url = ConfigurationManager.AppSettings["ApiUrl"];
+            config.EnableCors(new EnableCorsAttribute(url, headers: "*", methods: "*"));
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+               .Add(new MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
---
+++ MyDocuments/MyDocuments.PL/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyDocuments.BLL.Interfaces;
+using MyDocuments.BLL.DTO;
+
+namespace MyDocuments.PL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+           
+            return View();
+        }
+       
+
+    }
+}
